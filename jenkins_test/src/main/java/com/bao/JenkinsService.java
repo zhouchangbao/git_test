87bc4852dfc7c0f19e675491c23cbe7320ea0f47
zhouchangbao <zhouchangbao@tftiancai.com>
2015-04-30 21:50:00 +0800

    just a commit

---
+++ jenkins_test/src/main/java/com/bao/JenkinsService.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+/**
+ * Created by bao on 15/4/30.
+ */
+public class JenkinsService {
+
+  public void add() {
+    System.out.println("add a user");
+  }
+
+}
---
+++ jenkins_test/src/test/java/com/bao/JenkinsServiceTest.java
@@ -0,0 +1,12 @@
+package com.bao;
+
+/**
+ * Created by bao on 15/4/30.
+ */
+public class JenkinsServiceTest {
+
+  public void testAdd() {
+    System.out.println("----------");
+  }
+
+}
